7b3d736bc2f4435de923a83b3c3cec0e164093ce
kutluduman <kutluduman@gmail.com>
2021-03-17 22:50:24 -0400

    Initial packages

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Pet from "./components/Pet";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="dog" breed="Havanese" />
+      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="cat" breed="Mix" />
+    </div>
+  );
+};
+
+export default App;
