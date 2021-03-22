f0eef05ef797bfc821b39c80c8d46e66a2a27a36
kutluduman <kutluduman@gmail.com>
2021-03-22 14:32:40 -0400

    Hooks introduction with useState and eslint configuration for hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import Pet from "./components/Pet.jsx";
+import SearchParams from "./components/SearchParams.jsx";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="dog" breed="Havanese" />
-      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="cat" breed="Mix" />
+      <SearchParams />
     </div>
   );
 };
