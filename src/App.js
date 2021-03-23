a7318e084dc9f8865ad5a39ac3bea4ad3f11dc27
kutluduman <kutluduman@gmail.com>
2021-03-22 20:35:51 -0400

    Initial setup for react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import SearchParams from "./components/SearchParams.jsx";
+import Details from "./components/Details.jsx";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <SearchParams />
+      <Router>
+        <Switch>
+          <Route path="/details/:id">
+            <Details />
+          </Route>
+          <Route path="/">
+            <SearchParams />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 };
---
+++ src/components/Details.jsx
@@ -0,0 +1,5 @@
+const Details = () => {
+  return <h2>HI FROM DETAILS</h2>;
+};
+
+export default Details;
