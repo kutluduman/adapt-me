5378b49fbfda2bca19ec7e4033448e92526976ef
kutluduman <kutluduman@gmail.com>
2021-03-23 00:51:24 -0400

    Implemented the carousel component into details

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import { withRouter } from "react-router-dom";
+import Carousel from "./Carousel.jsx";
 
 class Details extends Component {
   state = { loading: true };
@@ -13,16 +14,23 @@ class Details extends Component {
   }
 
   render() {
-    console.log(this.state);
-
     if (this.state.loading) {
       return <h2>loading … </h2>;
     }
 
-    const { animal, breed, city, state, description, name } = this.state;
+    const {
+      animal,
+      breed,
+      city,
+      state,
+      description,
+      name,
+      images,
+    } = this.state;
 
     return (
       <div className="details">
+        <Carousel images={images} />
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} — ${breed} — ${city}, ${state}`}</h2>
